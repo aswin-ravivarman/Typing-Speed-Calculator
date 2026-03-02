a89b13843fe69eed9195cd50065aac9c85a11bae
Aswin <aswinravivarman@gmail.com>
2026-03-02 22:24:29 +0530

    Small change

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,7 +4,7 @@ function Stats({ timeLeft, wpm, accuracy }) {
   return (
     <div className="mb-3">
       <p><strong>Time Left:</strong> {timeLeft}s</p>
-      <p><strong>WPM:</strong> {wpm}</p>
+      <p><strong>Words Per Minute:</strong> {wpm}</p>
       <p><strong>Accuracy:</strong> {accuracy}%</p>
     </div>
   );
